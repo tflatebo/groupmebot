1942ee0d85249a4fd2ddbed7ec1e3a6a8e31e497
Tor Flatebo <tor.flatebo@govdelivery.com>
2015-03-22 11:06:47 -0500

    Echo any messaages back to the room

--- web.rb
+++ web.rb
@@ -1,16 +1,44 @@
 require 'sinatra'
 require 'json'
+require 'httparty'
+require 'logger'
+
+set :bot_uri, 'https://api.groupme.com/v3/bots/post'
+set :bot_id, ENV['GROUPMEBOTID']
+
+enable :logging
+
+helpers do
+  def post(text)
+    options = { :text => text, :bot_id => settings.bot_id }
+    logger.debug "Posting #{options[:text]} to groupme: "
+    gm_response = HTTParty.post(settings.bot_uri,
+                                :query => { :text => text, :bot_id => settings.bot_id },
+                                :debug_output => $stdout)
+
+    if(gm_response.success?)
+      logger.debug gm_response.response
+    else
+      logger.info "error posting to groupme bot: "
+      logger.info gm_response.response
+    end
+  end
+end
+
+before do
+  logger.level = Logger::DEBUG
+
+  request.body.rewind
+  @request_payload = JSON.parse request.body.read
+end
 
 get '/' do
   puts "Hello, world"
 end
 
 post '/' do
-  request.body.rewind
-  request_payload = JSON.parse request.body.read
 
-  #do something with request_payload
-  puts "Text from groupme: " + request_payload['text']
+  logger.debug "Text from groupme: " + @request_payload['text']
 
-  "Text from groupme: " + request_payload['text']
+  post('You said: ' + @request_payload['text'])
 end
